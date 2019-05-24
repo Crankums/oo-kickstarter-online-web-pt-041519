035449f4cfa9ca2c2d41adf28cc7e8820e617e90
Craig Donavin <crdonavin@gmail.com>
2019-05-24 21:44:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,7 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)
-    project = Project.new(project)
+  def back_project(project = Project.new(name))
     @backed_projects << project
   end
 
